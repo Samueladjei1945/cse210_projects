1e02d11b8d4132f9a5ad7737f6ee634c77462f5d
Samueladjei1945 <samueladjei1945@gmail.com>
2025-03-08 22:02:46 +0000

    New features added

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
        // Prompt the user for first name
-        Console.Write("What is your first name? ");
+        Console.Write("Please What is your first name? ");
         string firstName = Console.ReadLine();
 
         // Promptthe user for last name
-        Console.Write("What is your last name? ");
+        Console.Write("Please What is your last name? ");
         string lastName = Console.ReadLine();
 
         // Display the formatted name
